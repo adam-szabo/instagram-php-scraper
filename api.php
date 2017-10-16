9210fba38f19e67098bbc38ae4d6915a816a6d5f
Adam Szabo <adam.szabo.it@gmail.com>
2017-10-17 00:14:26 +0100

    Get account API endpoint

--- api.php
+++ api.php
@@ -9,14 +9,37 @@ use InstagramScraper\Instagram;
 
 Unirest\Request::verifyPeer(false);
 
+$supported_actions = 'Supported actions: "get_account", "get_followers".';
+
 if (empty($_GET['action'])) {
-    $error = 'Missing parameter "action". Supported actions: "get_followers".';
+    $error = 'Missing parameter "action". ' . $supported_actions;
     http_response_code(400);
     echo json_encode($error, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     return;
 }
 
 switch (strtolower($_GET['action'])) {
+    // ?action=get_account&username=USERNAME
+    case 'get_account':
+        // Validate input
+        if (empty($_GET['username'])) {
+            http_response_code(400);
+            echo json_encode('Missing query parameter "username".', JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+            return;
+        }
+
+        // Execute
+        try {
+            $account = Instagram::getAccount($_GET['username']);
+            echo json_encode($account, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+            return;
+        } catch (InstagramException $e) {
+            http_response_code(500);
+            echo json_encode($e, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+            return;
+        }
+        break;
+
     // basic auth with username and pass
     // ?action=get_followers&username=USERNAME&count=COUNT&page_size=PAGESIZE
     case 'get_followers':
@@ -61,9 +84,9 @@ switch (strtolower($_GET['action'])) {
             return;
         }
         break;
-        
+
     default:
-        $error = 'Invalid "action" parameter. Supported actions: "get_followers".';
+        $error = 'Invalid "action" parameter. ' . $supported_actions;
         http_response_code(400);
         echo json_encode($error, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
         return;
